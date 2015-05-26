3b80947cafc5725136cad2fe56341430fcf9f62d
stryku <stryku2393@gmail.com>
2015-05-27 01:51:24 +0200

    added simple doWork() definition to tests

--- include/VolumeChanger.hpp
+++ include/VolumeChanger.hpp
@@ -1,15 +1,27 @@
 #ifndef _VOLUMECHANGER_HPP_
 #define _VOLUMECHANGER_HPP_
 
-class VolumeChanger
+#include <Worker.hpp>
+
+#include <iostream>
+
+class VolumeChanger : public Worker
 {
+private:
+    struct WorkData
+    {
+        int8_t newVolumePercents;
+    };
+
 public:
     VolumeChanger( ) {}
     ~VolumeChanger( ) {}
 
     bool doWork( char *data )
     {
+        WorkData *workData = reinterpret_cast<WorkData*>( data );
 
+        std::cout << "Changing volume to: " << workData->newVolumePercents << "\n";
     }
 };
 
