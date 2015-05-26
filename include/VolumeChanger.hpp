7f8c2d46af9521a84bafe6ec15c4e402187499a3
stryku <stryku2393@gmail.com>
2015-05-27 01:40:49 +0200

    created VolumeChanger class

---
+++ include/VolumeChanger.hpp
@@ -0,0 +1,16 @@
+#ifndef _VOLUMECHANGER_HPP_
+#define _VOLUMECHANGER_HPP_
+
+class VolumeChanger
+{
+public:
+    VolumeChanger( ) {}
+    ~VolumeChanger( ) {}
+
+    bool doWork( char *data )
+    {
+
+    }
+};
+
+#endif
