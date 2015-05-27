91e8fd19a53da39d277b4dd08bf5c2dadd6ba5da
stryku <stryku2393@gmail.com>
2015-05-27 02:24:23 +0200

    created WorkersManager definition

--- include/WorkersManager.hpp
+++ include/WorkersManager.hpp
@@ -2,12 +2,48 @@
 #define _WORKERSMANAGER_HPP_
 
 #include <map>
+#include <memory>
 
 #include <Workers.h>
 
 class WorkersManager
 {
+private:
+    enum WorkerType
+    {
+        WT_VOLUME_CHANGER = 0,
+        WT_TEST
+    };
 
+    typedef  std::map<WorkerType, std::unique_ptr<Worker>> WorkersMap;
+    typedef  std::pair<WorkerType, std::unique_ptr<Worker>> WorkerPair;
+
+    WorkersMap workersMap;
+    
+    template <class WorkerClass>
+    void createWorker( WorkerType workerType )
+    {
+        workersMap.insert( WorkerPair( workerType,
+                                       std::make_unique<WorkerClass>() ) );
+    }
+
+    void initWorkersMap()
+    {
+        createWorker<VolumeChanger> ( WT_VOLUME_CHANGER );
+        createWorker<TestWorker>    ( WT_TEST );
+    }
+
+public:
+    WorkersManager() 
+    {
+        initWorkersMap();
+    }
+    ~WorkersManager() {}
+
+    bool doWork( WorkerType workerType, char *data )
+    {
+        return workersMap[workerType]->doWork( data );
+    }
 };
 
 #endif
