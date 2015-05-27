ab4a0809e3cf831868618862890443977406c825
stryku <stryku2393@gmail.com>
2015-05-28 01:33:06 +0200

    created WorkersFactory class

---
+++ include/WorkersFactory.hpp
@@ -0,0 +1,11 @@
+#ifndef _WORKERSFACTORY_HPP_
+#define _WORKERSFACTORY_HPP_
+
+class WorkersFactory
+{
+public:
+    WorkersFactory() {}
+    ~WorkersFactory() {}
+};
+
+#endif // _WORKERSFACTORY_HPP_
