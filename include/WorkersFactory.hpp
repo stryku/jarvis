a8f9438573dcb668880912b70c641cf92a52a013
stryku <stryku2393@gmail.com>
2015-05-28 01:37:19 +0200

    created WorkerType enum and createWorker() method

--- include/WorkersFactory.hpp
+++ include/WorkersFactory.hpp
@@ -1,11 +1,30 @@
 #ifndef _WORKERSFACTORY_HPP_
 #define _WORKERSFACTORY_HPP_
 
+#include <memory>
+
+#include <Workers.h>
+
 class WorkersFactory
 {
 public:
+    enum WorkerType
+    {
+        WT_TEST,
+        WT_VOLUME_CHANGER
+    };
+
     WorkersFactory() {}
     ~WorkersFactory() {}
+
+    static std::shared_ptr<Worker> createWorker( WorkerType type )
+    {
+        switch( type )
+        {
+            case WT_TEST:           return std::make_shared<TestWorker>();
+            case WT_VOLUME_CHANGER: return std::make_shared<VolumeChanger>();
+        }
+    }
 };
 
 #endif // _WORKERSFACTORY_HPP_
