7b9775bda904eee7647274f9897b34526bfd30ff
stryku <stryku2393@gmail.com>
2015-05-28 01:31:01 +0200

    created extractTaskType() method

--- include/XMLTaskParser.hpp
+++ include/XMLTaskParser.hpp
@@ -2,22 +2,44 @@
 #define _XMLTASKPARSER_HPP_
 
 #include <string>
+#include <map>
 
 #include <tinyxml/tinyxml2.h>
 
 #include <TaskTypeEnum.h>
 
+using namespace tinyxml2;
 
-struct XMLTaskParser
+
+class XMLTaskParser
 {
+private:
+    static std::map<std::string, TaskType> taskTypeMap;
+    static bool mapInitiali
+
+    TaskType extractTaskType( const char *xmlData )
+    {
+        XMLDocument doc( xmlData );
+        XMLElement* taskTypeElement;
+        const char *taskTypeString;
+
+        taskTypeElement = doc.FirstChildElement( "tasktype" );
+        taskTypeString = taskTypeElement->GetText();
+
+        return taskTypeMap[taskTypeString];
+    }
+
+    static
+
+public:
     TaskType taskType;
     std::string taskData;
 
     XMLTaskParser() {}
     XMLTaskParser( const char *xmlData ) 
     {
-        XMLDocument doc;
 
+        
         
     }
 
