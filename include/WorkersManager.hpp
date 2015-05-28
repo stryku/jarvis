78211426975d80db8f37ccab742e74d7179deda8
stryku <stryku2393@gmail.com>
2015-05-28 02:04:55 +0200

    now using new WorkersMap

--- include/WorkersManager.hpp
+++ include/WorkersManager.hpp
@@ -4,39 +4,16 @@
 #include <map>
 #include <memory>
 
-#include <Workers.h>
+#include <WorkersMap.hpp>
 
 class WorkersManager
 {
 private:
-    enum WorkerType
-    {
-        WT_VOLUME_CHANGER = 0,
-        WT_TEST
-    };
-
-    typedef  std::map<WorkerType, std::unique_ptr<Worker>> WorkersMap;
-    typedef  std::pair<WorkerType, std::unique_ptr<Worker>> WorkerPair;
-
     WorkersMap workersMap;
-    
-    template <class WorkerClass>
-    void createWorker( WorkerType workerType )
-    {
-        workersMap.insert( WorkerPair( workerType,
-                                       std::make_unique<WorkerClass>() ) );
-    }
-
-    void initWorkersMap()
-    {
-        createWorker<VolumeChanger> ( WT_VOLUME_CHANGER );
-        createWorker<TestWorker>    ( WT_TEST );
-    }
 
 public:
     WorkersManager() 
     {
-        initWorkersMap();
     }
     ~WorkersManager() {}
 
