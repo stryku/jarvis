5d76b3ba41886eaaa6f3acecc7687a483e2df07c
stryku <stryku2393@gmail.com>
2015-05-28 02:02:54 +0200

    make WorkersMap specialized ConstMap. Ner init() body

--- include/WorkersMap.hpp
+++ include/WorkersMap.hpp
@@ -2,15 +2,27 @@
 #define _WORKERSMAP_HPP_
 
 #include <ConstMap.hpp>
+#include <WorkersFactory.hpp>
 
-template <class KeyType, class ValueType>
-class WorkersMap : public ConstMap
+typedef WorkersFactory::WorkerType WorkerType;
+
+class WorkersMap : public ConstMap<WorkerType, std::shared_ptr<Worker>>
 {
 private:
 
+    void addWorker( WorkerType type )
+    {
+        map.insert( ElementPair( type,
+                                 WorkersFactory::createWorker( type ) ) );
+    }
+
     void init()
     {
+        WorkerType begin = WorkerType::WT_VOLUME_CHANGER,
+                   end = WorkerType::WT_END;
 
+        for( size_t type = begin; type != end; ++type )
+            addWorker( static_cast<WorkerType>( type ) );
     }
 
 public:
