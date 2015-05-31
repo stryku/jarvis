47512bf8e57441ba3d573758644b6e8940bf884c
stryku <stryku2393@gmail.com>
2015-06-01 00:38:11 +0200

    added default vector length and constructor, destructor

--- include/ReceivedMessage.hpp
+++ include/ReceivedMessage.hpp
@@ -3,9 +3,18 @@
 
 #include <vector>
 
-struct ReceivedMessage
+class ReceivedMessage
 {
+private:
+    static const size_t defaultVectorLength = 5 * 1024;
+
 public:
+    ReceivedMessage() : length( 0 )
+    {
+        message.resize( defaultVectorLength );
+    }
+    ~ReceivedMessage() {}
+
     std::vector<char> message;
     size_t length;
 };
