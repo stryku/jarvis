8ac918d0ce573c110d1ba5ca52f5b8122639cc5e
stryku <stryku2393@gmail.com>
2015-06-01 00:23:43 +0200

    created MessageSender class

---
+++ include/MessageSender.hpp
@@ -0,0 +1,25 @@
+#ifndef MESSAGESENDER_HPP
+#define MESSAGESENDER_HPP
+
+#include <string>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+class MessageSender
+{
+public:
+    static void sendMessage( tcp::socket &socket,
+                             const std::string &message )
+    {
+        boost::asio::async_write( socket,
+                                  boost::asio::buffer( message.c_str(), message.size() ),
+                                  []( boost::system::error_code ec ) 
+        {   
+            if( !ec )
+                throw boost::system::system_error( errorCode );
+        } );
+    }
+};
+
+#endif // MESSAGESENDER_HPP
