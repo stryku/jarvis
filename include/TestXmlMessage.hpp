420e7b6f9ec3a5f0d5ec0bd84dc66b7fc52bf00d
stryku <stryku2393@gmail.com>
2015-06-01 07:05:20 +0200

    creating message with message creator working now

--- include/TestXmlMessage.hpp
+++ include/TestXmlMessage.hpp
@@ -9,18 +9,10 @@ protected:
     void createDataNode( void *dataPtr )
     {
         DataStruct data( dataPtr );
-        auto dataNode = xmlDoc.allocate_node( node_element, "data" );
-        auto someIntString = std::to_string( data.someInt );
 
-        auto someIntNode = xmlDoc.allocate_node( node_element, "someint", someIntString.c_str() );
-        auto someCharNode = xmlDoc.allocate_node( node_element, "somechar", &data.someChar, 0, 1 );
-        auto someCharArrayNode = xmlDoc.allocate_node( node_element, "somechararray", data.someCharArray );
-        
-        dataNode->append_node( someIntNode );
-        dataNode->append_node( someCharNode );
-        dataNode->append_node( someCharArrayNode );
-
-        xmlDoc.first_node()->append_node( dataNode );
+        xmlMessageCreator.addDataElement( "someint", data.someInt );
+        xmlMessageCreator.addDataElement( "somechar", data.someChar );
+        xmlMessageCreator.addDataElement( "somechararray", data.someCharArray );
     }
 
 private:
