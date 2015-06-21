73668418fc4fd075e144ea183a0488d00b0d87a9
stryku <stryku2393@gmail.com>
2015-06-21 03:24:14 +0200

    remove buffer from client

--- include/Cient.hpp
+++ include/Cient.hpp
@@ -10,11 +10,8 @@ using boost::asio::ip::tcp;
 
 struct Client
 {
-    static const size_t defaultBufferSize = 1024;
-
     tcp::socket socket;
     std::string id;
-    std::array<char, defaultBufferSize> buffer;
 
     Client( tcp::socket socket ) :
         socket( std::move( socket ) )
