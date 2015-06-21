6bc77595d5ec6898b1374c017222629c6a5920c1
stryku <stryku2393@gmail.com>
2015-06-21 03:47:26 +0200

    created ClientManager class

---
+++ include/ClientManager.hpp
@@ -0,0 +1,25 @@
+#ifndef CLIENTMANAGER_HPP
+#define CLIENTMANAGER_HPP
+
+#include <Session.hpp>
+
+class ClientManager
+{
+private:
+    typedef std::shared_ptr<Session> SessionPtr;
+
+    std::set<SessionPtr> sessions;
+
+public:
+    ClientManager() {}
+    ~ClientManager() {}
+
+    void newClient( tcp::socket &socket )
+    {
+        auto session = std::make_shared<Session>( socket );
+        sessions.insert( session );
+        session->start();
+    }
+};
+
+#endif
