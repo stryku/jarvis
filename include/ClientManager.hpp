b34aff53d2ef6a5f039c49ec6c2699a119a870e6
stryku <stryku2393@gmail.com>
2015-06-21 04:32:03 +0200

    created clients maps

--- include/ClientManager.hpp
+++ include/ClientManager.hpp
@@ -1,14 +1,19 @@
 #ifndef CLIENTMANAGER_HPP
 #define CLIENTMANAGER_HPP
 
+#include <map>
+
 #include <Session.hpp>
 
 class ClientManager
 {
 private:
     typedef std::shared_ptr<Session> SessionPtr;
+    typedef std::shared_ptr<Client> ClientPtr;
 
     std::set<SessionPtr> sessions;
+    std::map<tcp::socket*, ClientPtr> socketClientMap;
+    std::map<std::string, ClientPtr> idClientMap;
 
 public:
     ClientManager() {}
@@ -16,9 +21,25 @@ public:
 
     void newClient( tcp::socket *socket )
     {
-        auto session = std::make_shared<Session>( socket );
+        auto client = std::make_shared<Client>( socket );
+        auto session = std::make_shared<Session>( client );
         sessions.insert( session );
         session->start();
+    }
+
+    void newClientId( const tcp::socket *clientSocket, const std::string &clientId )
+    {
+        idClientMap[clientId] = socketClientMap[clientSocket];
+    }
+
+    ClientPtr getClientBySocket( const tcp::socket *clientSocket )
+    {
+        return socketClientMap[clientSocket];
+    }
+
+    ClientPtr getClientById( const std::string &clientId )
+    {
+        return idClientMap[clientId];
     }
 };
 
