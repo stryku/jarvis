e95a8cc403f996d4aaddf27b221028cb98716a82
stryku <stryku2393@gmail.com>
2015-06-21 03:50:32 +0200

    cleaned Server.hpp code

--- include/Server.hpp
+++ include/Server.hpp
@@ -8,15 +8,29 @@
 #include <RawMessage.hpp>
 #include <MessageManager.hpp>
 
-#include <Session.hpp>
+//#include <Session.hpp>
+#include <ClientManager.hpp>
 
 using boost::asio::ip::tcp;
 
 class Server
 {
 private:
-    std::array<char, 2048> data;
-    std::set<std::shared_ptr<Session>> sessions;
+    ClientManager clientManager;
+    tcp::socket socket;
+    tcp::acceptor acceptor_;
+
+    void do_accept( )
+    {
+        acceptor_.async_accept( socket,
+                                [this]( boost::system::error_code ec )
+        {
+            if( !ec )
+                clientManager.newClient( socket );
+
+            do_accept( );
+        } );
+    }
 
 public:
     Server( boost::asio::io_service& io_service,
@@ -27,84 +41,6 @@ public:
         do_accept();
     }
     ~Server() {}
-
-    /*int32_t readMessageLength()
-    {
-        const size_t bufSize = 20;
-
-        char buf[bufSize];
-        int32_t receivedLength;
-        boost::system::error_code errorCode;
-        
-
-        socket.read_some( boost::asio::buffer( buf, bufSize ),
-                          errorCode );
-
-        if( errorCode )
-            throw boost::system::system_error( errorCode );
-
-        return atoi( buf );
-    }
-
-    size_t readPartOfMessage( boost::system::error_code &ec )
-    {
-        return socket.read_some( boost::asio::buffer( receivedMessage.data ),
-                                 ec );
-    }
-
-    void readMessage()
-    {
-        boost::system::error_code errorCode;
-        bool extractedSize = false;
-        size_t readedSize = 0;
-        int32_t messageLength;
-
-        messageLength = readMessageLength();
-
-        do
-        {
-            readedSize += readPartOfMessage( errorCode );
-
-        } while( !errorCode && readedSize < messageLength );
-
-        if( errorCode )
-            throw boost::system::system_error( errorCode );
-
-
-        receivedMessage.socketPtr = &socket;
-        receivedMessage.length = messageLength;
-        receivedMessage.data[receivedMessage.length] = '\0';
-    }
-
-    void readMessages()
-    {
-        for( ;; )
-        {
-            readMessage();
-            messageManager.receivedNewMessage( receivedMessage );
-        }
-    }*/
-
-    void do_accept( )
-    {
-        acceptor_.async_accept( socket,
-                                [this]( boost::system::error_code ec )
-        {
-            if( !ec )
-            {
-
-                auto session = std::make_shared<Session>( std::move( socket ) );
-                sessions.insert( session );
-                session->start();
-            }
-
-            do_accept( );
-        } );
-    }
-
-    //MessageManager messageManager;
-    tcp::socket socket;
-    tcp::acceptor acceptor_;
 };
 
 #endif
