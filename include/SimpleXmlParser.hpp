220f476b0464b76677e3a204b716565f3842a75d
stryku <stryku2393@gmail.com>
2015-07-19 17:19:22 +0200

    added extractNode() method

--- include/SimpleXmlParser.hpp
+++ include/SimpleXmlParser.hpp
@@ -1,12 +1,12 @@
 #ifndef SIMPLEXMLPARSER_HPP
 #define SIMPLEXMLPARSER_HPP
 
+#include <rapidxml\rapidxml.hpp>
+#include <rapidxml\rapidxml_print.hpp>
+
 #include <string>
 #include <vector>
 #include <memory>
-
-#include <rapidxml\rapidxml.hpp>
-#include <rapidxml\rapidxml_print.hpp>
 
 class SimpleXmlParser
 {
@@ -17,6 +17,15 @@ public:
         doc.parse<0>( const_cast<char*>( xmlData.c_str() ) );
 
         return std::string( doc.value() );
+    }
+
+    static std::string extractNode( const char *nodeName, 
+                                    const char *xmlData )
+    {
+        xml_document <> doc;
+        doc.parse<0>( const_cast<char*>( xmlData ) );
+
+        return std::string( doc.first_node( nodeName )->value() );
     }
 };
 
