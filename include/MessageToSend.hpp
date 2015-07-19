4f2a4f3739fd217ef118c10adb055f3b9550f25d
stryku <stryku2393@gmail.com>
2015-07-19 15:25:25 +0200

    created MessageToSend clss

---
+++ include/MessageToSend.hpp
@@ -0,0 +1,12 @@
+#ifndef MESSAGETOSEND_HPP
+#define MESSAGETOSEND_HPP
+
+#include <PersonalMessage.hpp>
+
+struct MessageToSend
+{
+    std::string id;
+    PersonalMessage personalMsg;
+};
+
+#endif
