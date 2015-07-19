60896ddcd3a9dabff7b88c4237d7e8d13f445d7b
stryku <stryku2393@gmail.com>
2015-07-19 15:40:01 +0200

    created ThreadSafeHeap class

---
+++ include/ThreadSafeHeap.hpp
@@ -0,0 +1,59 @@
+#ifndef THREADSAFEHEAP_HPP
+#define THREADSAFEHEAP_HPP
+
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+template <typename T>
+class ThreadSafeHeap
+{
+public:
+    T pop( )
+    {
+        std::unique_lock<std::mutex> mlock( mutex_ );
+
+        while( queue_.empty( ) )
+            cond_.wait( mlock );
+
+        auto item = queue_.top( );
+        queue_.pop( );
+        return item;
+    }
+
+    void pop( T& item )
+    {
+        std::unique_lock<std::mutex> mlock( mutex_ );
+        while( queue_.empty( ) )
+        {
+            cond_.wait( mlock );
+        }
+        item = queue_.top( );
+        queue_.pop( );
+    }
+
+    void push( const T& item )
+    {
+        std::unique_lock<std::mutex> mlock( mutex_ );
+        queue_.push( item );
+        mlock.unlock( );
+        cond_.notify_one( );
+    }
+
+    void push( T&& item )
+    {
+        std::unique_lock<std::mutex> mlock( mutex_ );
+        queue_.push( std::move( item ) );
+        mlock.unlock( );
+        cond_.notify_one( );
+    }
+
+private:
+    std::priority_queue<T> queue_;
+    std::mutex mutex_;
+    std::condition_variable cond_;
+};
+
+
+#endif
