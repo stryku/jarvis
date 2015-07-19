91b0ed3199c1df89d7020348e74ceb269706b98e
stryku <stryku2393@gmail.com>
2015-07-19 15:29:22 +0200

    added and changed constructors

--- include/PersonalMessage.hpp
+++ include/PersonalMessage.hpp
@@ -1,17 +1,27 @@
 #ifndef SERVERREQUEST_HPP
 #define SERVERREQUEST_HPP
 
+#include <memory>
+
 #include <zmq.hpp>
 
 struct PersonalMessage
 {
+    typedef std::shared_ptr<zmq::message_t> ZmqMessagePtr;
+
     zmq::message_t identity;
     zmq::message_t msg;
 
-    PersonalMessage( zmq::message_t &identity_, zmq::message_t &msg_ )
+    PersonalMessage( const zmq::message_t &identity_, const zmq::message_t &msg_ )
     {
         identity.copy( &identity_ );
         msg.copy( &msg_ );
+    }
+
+    PersonalMessage( const zmq::message_t &identity_, const ZmqMessagePtr &msg_ )
+    {
+        identity.copy( &identity_ );
+        msg.copy( msg_.get() );
     }
 
     PersonalMessage( const PersonalMessage &req )
